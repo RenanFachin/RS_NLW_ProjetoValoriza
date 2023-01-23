f168842dfbd600ff5b8d243cc6cc2815d02bac12
Renan <renan.fachin@hotmail.com>
2023-01-23 17:04:06 -0300

    adds data validation in the controller with ZOD and exception handling

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+
 import { CreateUserService } from '../services/CreateUserService'
 
 class CreateUserController {
     async handle(request: Request, response: Response) {
+        // Validação de dados de entrada com o zod
+        const createUserBody = z.object({
+            name: z.string(),
+            email: z.string(),
+            admin: z.boolean()
+        })
+
         // Recuperando as informações de dentro da requisição
-        const { name, email, admin } = request.body;
+        const { name, email, admin } = createUserBody.parse(request.body)
 
         // instânciando o service
         const createUserService = new CreateUserService()
 
         // Passando as infos para dentro do service
-        const user = await createUserService.execute({name, email, admin})
+        const user = await createUserService.execute({ name, email, admin })
 
         return response.status(201).json(user)
     }
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@
 import 'dotenv/config'
 
 // Express
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+
+import "express-async-errors"
 
 // importar o router
 import { router } from './routes'
@@ -15,6 +17,18 @@ app.use(express.json())
 
 // Inicializar as rotas
 app.use(router)
+
+
+// Middleware de erro
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    // Verificando se é um erro "tratado" pela aplicação (throw new error)
+    if (err instanceof Error) {
+        return response.status(400).json({ error: err.message })
+    }
+
+    return response.status(500).json({ status: "error", message: "Internal Server Error" })
+})
+
 
 const PORT = process.env.PORT
 // Criando o servidor
